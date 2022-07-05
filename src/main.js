eb2884930017c035c81409ba6c27eeba023a391c
Gustav Sneholm Bruhns <gustavsneholmbruhns@Gustavs-MacBook-Pro.local>
2022-07-05 18:41:58 +0200

    simplified

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 const db = require('../DatabaseScripts/databaseConnect');
-const prompt = require('prompt-sync')();
 const fs = require('fs');
-
 
 function main() {
     try {
-        // Get (assumed) select query from user
-        let q = prompt("Enter query please: ");
+        let q = "SELECT * FROM users;";
         // Run query in Database db, write contents to file, delete retrieved rows from db    
         getAndDeleteRowsWithBackup(q); 
     }
@@ -16,52 +13,40 @@ function main() {
 }
 
 function getAndDeleteRowsWithBackup(query) {
-    db.all(query, [], (e, rows) => {
+    db.all(query, [], (e, users) => {
         if (e) {
             console.log("Error occured while attempting to query database: \n");
             throw e;
         }
         
         if (fs.existsSync("BackUp.txt")) {
-            fs.writeFileSync("BackUp.txt", JSON.stringify(rows, null, 4));
+            fs.writeFileSync("BackUp.txt", JSON.stringify(users, null, 4));
             if (checkDataIntegrity(rows) === true) {
                 console.log("Data was correctly written to backup file.");
+                deleteUsers(users);
             } else {
                 console.log("Data was integrity was not maintained.");
             }
-            deleteRows(rows);
+        } else {
+            console.log("No file was found for the following path: " + __dirname + "/BackUp.txt");
         }
-        console.log("No file was found for the following path: " + __dirname + "/BackUp.txt");
     });
 }
 
-function checkDataIntegrity(rows) {
+function checkDataIntegrity(users) {
     const path = __dirname + "/BackUp.txt";
-    const contents = fs.readFileSync(path);
-    const originalContent = JSON.stringify(rows);
-        
+    const writtenUsers = fs.readFileSync(path);
+    const dbUsers = JSON.stringify(users);
+
     // Replace all white spaces and then compare strings.
-    if (originalContent.replace(/\s/g, "") === contents.toString().replace(/\s/g, "")) {
+    if (dbUsers.replace(/\s/g, "") === writtenUsers.toString().replace(/\s/g, "")) {
         return true;
     }
     return false;
 }
 
-function deleteRows(rows) {
-    if (rows.length !== 0) {
-        rowKeys = Object.keys();
-
-        rows.forEach(row => {
-            let condition = "";
-            rowKeys.forEach(key => {
-                condition += `${key} = "${row[key]}" AND `;
-            });
-            
-            let i = condition.lastIndexOf("AND");
-            condition = condition.slice(0, i);
-            db.run(`DELETE FROM users WHERE ${condition};`, (err) => {if(err) {console.error(err.message)}}); 
-        });
-    }
+function deleteUsers(users) {
+    db.run("DELETE FROM users;");   
 }
 
 main();
